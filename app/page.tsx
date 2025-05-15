ae8fa4866b2456a21be3b71e17db970042367ce6
Abdulrazaq Salihu <abdrzq.salihu@gmail.com>
2025-05-15 03:04:27 +0100

    feat: add state management for filters and sorting in ProductListings
    This commit introduces state management for filters and sorting in the ProductListings component. The `useState` hook is used to manage filter categories, product types, price ranges, and the sorting criteria. This enhancement allows for dynamic filtering and sorting of products based on user preferences.

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,31 @@
+"use client";
+
 import Link from "next/link";
 import Cta from "./components/Cta";
 import Features from "./components/Features";
 import Hero from "./components/Hero";
 import Newsletter from "./components/Newsletter";
 import ProductListings from "./components/ProductListings";
+import { useState } from "react";
 
 export default function Home() {
+  const [filters] = useState<{
+    category: string[];
+    productType: string[];
+    price: string[];
+  }>({
+    category: [],
+    productType: [],
+    price: [],
+  });
+
+  const [sortBy] = useState<string>("Date Added");
   return (
     <div>
       <Hero />
       <Features />
       <div className="mt-14">
-        <ProductListings limit={4} />
+        <ProductListings filters={filters} sortBy={sortBy} limit={4} />
       </div>
       <div className="flex justify-center">
         <Link
